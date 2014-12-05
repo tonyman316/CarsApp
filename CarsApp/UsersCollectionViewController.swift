42ae82c747d5da2cb6fe55371f31c4000fd1ba74
boboborghi <bobo.borghi@me.com>
2014-12-04 16:43:26 -0800

    Collection View Flow Layout Created

--- CarsApp/UsersCollectionViewController.swift
+++ CarsApp/UsersCollectionViewController.swift
@@ -19,7 +19,7 @@ class UsersCollectionViewController: UICollectionViewController, UICollectionVie
     //        var value = collectionView.frame.height - collectionView.layoutMargins.top * 3
     //        return CGSizeMake(value, value)
     //    }
-    
+        
     func animateCollectionViewAppearance() {
         UIView.animateWithDuration(1.5, delay: 0.5, usingSpringWithDamping: 0.5, initialSpringVelocity: 1, options: nil, animations: { () -> Void in
             self.collectionView!.contentOffset = CGPointMake(self.collectionView!.frame.size.width, 0)
