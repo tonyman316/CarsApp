d8346a59b5001022703e380e77200d97ac76c1e3
Tony <tonyman613@yahoo.com>
2014-12-06 10:09:16 -0800

    setting but not finish

--- CarsApp/SettingTableViewController.swift
+++ CarsApp/SettingTableViewController.swift
@@ -11,13 +11,19 @@ import Foundation
 import CoreData
 
 class SettingTableViewController: UITableViewController {
+    let managedObjectContext = (UIApplication.sharedApplication().delegate as AppDelegate).managedObjectContext
 
+    var sectionNames = [String]()
     let identifier = "settingCell"
-    
+        
     override func viewDidLoad() {
         super.viewDidLoad()
+        sectionNames.append("Units")
+        sectionNames.append("Oil Change Frequency")
 
         tableView.registerNib(UINib(nibName:"SettingTableViewCell", bundle: nil), forCellReuseIdentifier: identifier)
+        tableView.registerClass(UITableViewHeaderFooterView.self, forHeaderFooterViewReuseIdentifier: "headerCell")
+
     }
 
     override func didReceiveMemoryWarning() {
@@ -28,13 +34,46 @@ class SettingTableViewController: UITableViewController {
     // MARK: - Table view data source
 
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        return 2
+        return sectionNames.count
     }
 
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
 
         return 1
     }
+    
+    override func tableView(tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 30.0
+    }
+    
+//    override func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+//        switch(section){
+//        case 0: title = "Units (Mile/Km)"
+//        case 1: title = "Oil Change Freqency"
+//        case 2: title = "Transmission Oil Change Frequency"
+//        default: title = ""
+//        }
+//        return title
+//    }
+    
+    override func tableView(tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let cell = tableView.dequeueReusableHeaderFooterViewWithIdentifier("headerCell") as UITableViewHeaderFooterView!
+
+        var headerLabel = UILabel()
+        headerLabel.tag = 999
+        
+        cell.contentView.addSubview(headerLabel)
+        
+        //headerLabel.text = "test"
+        
+//        lab.font = UIFont(name:"Georgia-Bold", size:22)
+//        lab.textColor = UIColor.greenColor()
+//        lab.backgroundColor = UIColor.clearColor()
+//        h.contentView.addSubview(lab)
+   
+        return cell
+    }
+    
 
     
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
