aa77d937719a2fd12edc264c27b377c3df1e6e73
mirzamo <m.mirzamo@gmail.com>
2014-12-07 13:11:07 -0800

    //

--- CarsApp/CarCollectionCollectionViewController.swift
+++ CarsApp/CarCollectionCollectionViewController.swift
@@ -6,94 +6,184 @@
 //  Copyright (c) 2014 DeAnza. All rights reserved.
 //
 
+//import UIKit
+
+//let reuseIdentifier = "Cell"
+
+//class CarCollectionCollectionViewController: UICollectionViewController {
+//    
+//    var cars: [MyCars]?
+//
+//
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//
+//        // Uncomment the following line to preserve selection between presentations
+//        // self.clearsSelectionOnViewWillAppear = false
+//
+//        // Register cell classes
+//        self.collectionView!.registerClass(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+//
+//        // Do any additional setup after loading the view.
+//    }
+//
+//    override func didReceiveMemoryWarning() {
+//        super.didReceiveMemoryWarning()
+//        // Dispose of any resources that can be recreated.
+//    }
+//
+//    /*
+//    // MARK: - Navigation
+//
+//    // In a storyboard-based application, you will often want to do a little preparation before navigation
+//    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+//        // Get the new view controller using [segue destinationViewController].
+//        // Pass the selected object to the new view controller.
+//    }
+//    */
+//
+//    // MARK: UICollectionViewDataSource
+//
+//    override func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+//        //#warning Incomplete method implementation -- Return the number of sections
+//        return 1
+//    }
+//
+//
+//    override func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+//        if let array = cars {
+//        return array.count
+//        }
+//        return 0
+//    }
+//
+//    override func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+//        let cell = collectionView.dequeueReusableCellWithReuseIdentifier(reuseIdentifier, forIndexPath: indexPath) as UICollectionViewCell
+//    
+//        // Configure the cell
+//    
+//        return cell
+//    }
+//
+//    // MARK: UICollectionViewDelegate
+//
+//    /*
+//    // Uncomment this method to specify if the specified item should be highlighted during tracking
+//    override func collectionView(collectionView: UICollectionView, shouldHighlightItemAtIndexPath indexPath: NSIndexPath) -> Bool {
+//        return true
+//    }
+//    */
+//
+//    /*
+//    // Uncomment this method to specify if the specified item should be selected
+//    override func collectionView(collectionView: UICollectionView, shouldSelectItemAtIndexPath indexPath: NSIndexPath) -> Bool {
+//        return true
+//    }
+//    */
+//
+//    /*
+//    // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
+//    override func collectionView(collectionView: UICollectionView, shouldShowMenuForItemAtIndexPath indexPath: NSIndexPath) -> Bool {
+//        return false
+//    }
+//
+//    override func collectionView(collectionView: UICollectionView, canPerformAction action: Selector, forItemAtIndexPath indexPath: NSIndexPath, withSender sender: AnyObject?) -> Bool {
+//        return false
+//    }
+//
+//    override func collectionView(collectionView: UICollectionView, performAction action: Selector, forItemAtIndexPath indexPath: NSIndexPath, withSender sender: AnyObject?) {
+//    
+//    }
+//    */
+//
+//}
+
 import UIKit
 
-let reuseIdentifier = "Cell"
-
-class CarCollectionCollectionViewController: UICollectionViewController {
+class CarCollectionViewController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
+    let reuseIdentifier = "customCell"
+    let managedObjectContext = (UIApplication.sharedApplication().delegate as AppDelegate).managedObjectContext
+    var cars: [MyCars]? {
+        didSet {
+            collectionView!.reloadData()
+        }
+    }
+    var selectedCars: [MyCars]?
+    var del: SelectCarsDelegate?
+    let colorForBorder = UIColor(red:(179.0/255.0), green:(179.0/255.0), blue:(179.0/255.0), alpha:(0.3)).CGColor
     
-    var cars: [MyCars]?
-
-
+    func animateCollectionViewAppearance() {
+        UIView.animateWithDuration(1.5, delay: 0.5, usingSpringWithDamping: 0.5, initialSpringVelocity: 1, options: nil, animations: { () -> Void in
+            self.collectionView!.contentOffset = CGPointMake(self.collectionView!.frame.size.width, 0)
+            }, completion: nil)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Register cell classes
-        self.collectionView!.registerClass(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
-
-        // Do any additional setup after loading the view.
+        collectionView!.alwaysBounceHorizontal = true
+        collectionView!.backgroundColor = nil
+        animateCollectionViewAppearance()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        collectionView!.reloadData()
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using [segue destinationViewController].
-        // Pass the selected object to the new view controller.
-    }
-    */
-
-    // MARK: UICollectionViewDataSource
-
+    
     override func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
-        //#warning Incomplete method implementation -- Return the number of sections
         return 1
     }
-
-
+    
     override func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         if let array = cars {
-        return array.count
+            return array.count
         }
+        
         return 0
     }
-
-    override func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCellWithReuseIdentifier(reuseIdentifier, forIndexPath: indexPath) as UICollectionViewCell
     
-        // Configure the cell
-    
+    override func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> CarsCollectionViewCell {
+        var cell = collectionView.dequeueReusableCellWithReuseIdentifier(reuseIdentifier, forIndexPath: indexPath) as CarsCollectionViewCell
+        cell.carImageView.image = UIImage(data: cars![indexPath.row].carImage)
+        cell.nameLabel.text = cars![indexPath.row].model
+        
+        let selected = cars![indexPath.row]
+        
+        if selectedCars != nil {
+            if contains(selectedCars!, selected) == true {
+                cell.carImageView.layer.borderColor = UIColor.blueColor().CGColor
+            } else {
+                cell.carImageView.layer.borderColor = colorForBorder
+            }
+        }
+        
         return cell
     }
-
-    // MARK: UICollectionViewDelegate
-
-    /*
-    // Uncomment this method to specify if the specified item should be highlighted during tracking
-    override func collectionView(collectionView: UICollectionView, shouldHighlightItemAtIndexPath indexPath: NSIndexPath) -> Bool {
-        return true
-    }
-    */
-
-    /*
-    // Uncomment this method to specify if the specified item should be selected
-    override func collectionView(collectionView: UICollectionView, shouldSelectItemAtIndexPath indexPath: NSIndexPath) -> Bool {
-        return true
-    }
-    */
-
-    /*
-    // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
-    override func collectionView(collectionView: UICollectionView, shouldShowMenuForItemAtIndexPath indexPath: NSIndexPath) -> Bool {
-        return false
-    }
-
-    override func collectionView(collectionView: UICollectionView, canPerformAction action: Selector, forItemAtIndexPath indexPath: NSIndexPath, withSender sender: AnyObject?) -> Bool {
-        return false
-    }
-
-    override func collectionView(collectionView: UICollectionView, performAction action: Selector, forItemAtIndexPath indexPath: NSIndexPath, withSender sender: AnyObject?) {
     
+    override func viewWillDisappear(animated: Bool) {
+        super.viewWillDisappear(animated)
+        del?.didSelectCars(self, selectedCars: selectedCars)
     }
-    */
-
+    
+    override func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
+        if parentViewController is AddCarsViewController {
+            let selected = cars![indexPath.row]
+            
+            var cell = collectionView.cellForItemAtIndexPath(indexPath) as CarsCollectionViewCell
+            
+            if selectedCars != nil && contains(selectedCars!, selected) == true {
+                cell.carImageView.layer.borderColor = colorForBorder
+                selectedCars!.removeAtIndex(find(selectedCars!, selected)!)
+            } else {
+                if selectedCars == nil {
+                    selectedCars = [MyCars]()
+                }
+                
+                cell.carImageView.layer.borderColor = UIColor.blueColor().CGColor
+                selectedCars!.append(selected)
+            }
+            
+            del?.didSelectCars(self, selectedCars: selectedCars)
+        }
+    }
 }
