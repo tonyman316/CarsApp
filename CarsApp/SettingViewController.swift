e0ea2a084ae3678bb3af86e254fcb24b0c61a4d7
Tony <tonyman613@yahoo.com>
2014-12-07 16:07:52 -0800

    almost fix setting

--- CarsApp/SettingViewController.swift
+++ CarsApp/SettingViewController.swift
@@ -17,89 +17,91 @@ class SettingViewController: UIViewController, NSFetchedResultsControllerDelegat
     @IBOutlet var oilChangeSegmentControl: UISegmentedControl!
     @IBOutlet var transmissionSegmentControl: UISegmentedControl!
     
-//    var userUnit = ""
-//    var userOilChange = 0
-//    var userTransmission = 0
-    
     var setting: Setting? = nil
     var settingFromDatabase = Setting.databaseContainsSettings((UIApplication.sharedApplication().delegate as AppDelegate).managedObjectContext!).settings
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        if setting == nil {
+        if settingFromDatabase == nil {
+            println("No existing settings")
             createDefaultSetting()
         } else {
+            println("Found existing settings")
             setting = settingFromDatabase!
-        }
-    }
-    
-    override func viewWillAppear(animated: Bool) {
-        super.viewWillAppear(animated)
-        
-        // Show correct Segments
-        if settingFromDatabase?.unit == "miles" {
-            unitSegmentControl.selectedSegmentIndex = 0
-        }else{
-            unitSegmentControl.selectedSegmentIndex = 1
+            updateSegmentInterface()
         }
         
-        if settingFromDatabase?.oilChangeFrequency == 5000 {
-            oilChangeSegmentControl.selectedSegmentIndex = 0
-        }else{
-            oilChangeSegmentControl.selectedSegmentIndex = 1
-        }
-        
-        if settingFromDatabase?.transmissionFluidFrequency == 30000 {
-            transmissionSegmentControl.selectedSegmentIndex = 0
-        }else{
-            transmissionSegmentControl.selectedSegmentIndex = 1
-        }
+        unitSegmentControl.addTarget(self, action: "segmentValueChanged:", forControlEvents: UIControlEvents.ValueChanged)
+        oilChangeSegmentControl.addTarget(self, action: "segmentValueChanged:", forControlEvents: UIControlEvents.ValueChanged)
+        transmissionSegmentControl.addTarget(self, action: "segmentValueChanged:", forControlEvents: UIControlEvents.ValueChanged)
         
         println("settingFromDatabase: \(settingFromDatabase?.unit) \(settingFromDatabase?.oilChangeFrequency) \(settingFromDatabase?.transmissionFluidFrequency)")
-        
+        println("setting: \(setting?.unit) \(setting?.oilChangeFrequency) \(setting?.transmissionFluidFrequency)")
+
+    }
+    
+    func segmentValueChanged(segment: UISegmentedControl) {
+        if segment == unitSegmentControl {
+            if (unitSegmentControl.selectedSegmentIndex == 0){
+                setting?.unit = "miles"
+            } else if (unitSegmentControl.selectedSegmentIndex == 1){
+                setting?.unit = "km"
+            }
+            
+        } else if segment == oilChangeSegmentControl {
+            if (oilChangeSegmentControl.selectedSegmentIndex == 0){
+                setting?.oilChangeFrequency = 5000
+            } else if (oilChangeSegmentControl.selectedSegmentIndex == 1){
+                setting?.oilChangeFrequency = 8000
+            }
+            
+        } else if segment == transmissionSegmentControl {
+            if (transmissionSegmentControl.selectedSegmentIndex == 0){
+                setting?.oilChangeFrequency = 30000
+            } else if (transmissionSegmentControl.selectedSegmentIndex == 1){
+                setting?.oilChangeFrequency = 60000
+            }
+        }
     }
     
     override func viewWillDisappear(animated: Bool) {
         super.viewWillDisappear(animated)
         
         // Save settings
-        //settingFromDatabase?.unit = setting?.unit
-        //setting?.unit = userUnit
-        println("viewWillApper:\(settingFromDatabase?.unit)")
-        //println("setting.unit at view will disapper \(setting?.unit)")
-        
         managedObjectContext?.save(nil)
-
     }
     
     @IBAction func unitSegment(sender: AnyObject) {
-                
+        
         switch (unitSegmentControl.selectedSegmentIndex) {
         case 0: setting?.unit = "miles"
-                oilChangeSegmentControl.setTitle("5000 miles", forSegmentAtIndex: 0)
-                oilChangeSegmentControl.setTitle("8000 miles", forSegmentAtIndex: 1)
-                transmissionSegmentControl.setTitle("30000 miles", forSegmentAtIndex: 0)
-                transmissionSegmentControl.setTitle("60000 miles", forSegmentAtIndex: 1)
-                println("UnitSegment 0, userUnit: \(setting?.unit)")
-    
+        oilChangeSegmentControl.setTitle("5000 miles", forSegmentAtIndex: 0)
+        oilChangeSegmentControl.setTitle("8000 miles", forSegmentAtIndex: 1)
+        transmissionSegmentControl.setTitle("30000 miles", forSegmentAtIndex: 0)
+        transmissionSegmentControl.setTitle("60000 miles", forSegmentAtIndex: 1)
+        println("UnitSegment 0, userUnit: \(setting?.unit)")
+            
         case 1: setting?.unit = "km"
-                oilChangeSegmentControl.setTitle("\(UnitConverter.milesToKilometers(5000)) km", forSegmentAtIndex: 0)
-                oilChangeSegmentControl.setTitle("\(UnitConverter.milesToKilometers(8000)) km", forSegmentAtIndex: 1)
-                transmissionSegmentControl.setTitle("\(UnitConverter.milesToKilometers(30000)) km", forSegmentAtIndex: 0)
-                transmissionSegmentControl.setTitle("\(UnitConverter.milesToKilometers(60000)) km", forSegmentAtIndex: 1)
-                println("UnitSegment 1, userUnit: \(setting?.unit)")
-
+        oilChangeSegmentControl.setTitle("\(UnitConverter.milesToKilometers(5000)) km", forSegmentAtIndex: 0)
+        oilChangeSegmentControl.setTitle("\(UnitConverter.milesToKilometers(8000)) km", forSegmentAtIndex: 1)
+        transmissionSegmentControl.setTitle("\(UnitConverter.milesToKilometers(30000)) km", forSegmentAtIndex: 0)
+        transmissionSegmentControl.setTitle("\(UnitConverter.milesToKilometers(60000)) km", forSegmentAtIndex: 1)
+        println("UnitSegment 1, userUnit: \(setting?.unit)")
+            
         default:
             break;
         }
     }
-
+    
     @IBAction func oilChangeSegment(sender: AnyObject) {
         
-        switch (unitSegmentControl.selectedSegmentIndex) {
-//        case 0: setting?.oilChangeFrequency = 5000
-//        case 1: setting?.oilChangeFrequency = 8000
+        switch (oilChangeSegmentControl.selectedSegmentIndex) {
+        case 0: setting?.oilChangeFrequency = 5000
+                println("UnitSegment 0, userOilChange: \(setting?.oilChangeFrequency)")
+
+        case 1: setting?.oilChangeFrequency = 8000
+                println("UnitSegment 1, userOilChange: \(setting?.oilChangeFrequency)")
             
         default:
             break;
@@ -108,14 +110,17 @@ class SettingViewController: UIViewController, NSFetchedResultsControllerDelegat
     
     @IBAction func transmissionSegment(sender: AnyObject) {
         
-        switch (unitSegmentControl.selectedSegmentIndex) {
-//        case 0: setting?.transmissionFluidFrequency = 30000
-//        case 1: setting?.transmissionFluidFrequency = 60000
+        switch (transmissionSegmentControl.selectedSegmentIndex) {
+        case 0: setting?.transmissionFluidFrequency = 30000
+                println("UnitSegment 0, userTran: \(setting?.transmissionFluidFrequency)")
+
+        case 1: setting?.transmissionFluidFrequency = 60000
+                println("UnitSegment 1, userTran: \(setting?.transmissionFluidFrequency)")
 
         default:
             break;
         }
-
+        
     }
     
     // Save to Core Data
@@ -125,37 +130,44 @@ class SettingViewController: UIViewController, NSFetchedResultsControllerDelegat
         let entityDescripition = NSEntityDescription.entityForName("Setting", inManagedObjectContext: managedObjectContext!)
         newSetting = Setting(entity: entityDescripition!, insertIntoManagedObjectContext: managedObjectContext)
         
-            newSetting.unit = "miles"
-            newSetting.oilChangeFrequency = 5000
-            newSetting.transmissionFluidFrequency = 30000
-        
-//        if var oilChangeMiles = (oilChangeLabel.text? as NSString).doubleValue {
-//            newDefaultSetting.oilChangeFrequency = oilChangeMiles
-//            //println("oilChangeMiles: \(newDefaultSetting.oilChangeFrequency)")
-//        }
-//        
-//        if var tranMiles = transmissionLabel.text?.toInt() {
-//            newDefaultSetting.transmissionFluidFrequency = tranMiles
-//            //println("tranMiles: \(newDefaultSetting.transmissionFluidFrequency)")
-//        }
+        newSetting.unit = "miles"
+        newSetting.oilChangeFrequency = 5000
+        newSetting.transmissionFluidFrequency = 30000
         
         managedObjectContext?.save(nil)
+        
+        setting = newSetting
+        
+        println("New settings created: \(newSetting)")
+        
+        updateSegmentInterface()
     }
     
-    // Update to CoreData
-    func updateSettingFromUser() {
-        var userSetting: Setting
+    func updateSegmentInterface() {
+        // Show correct Segments
+        if setting?.unit == "miles" {
+            unitSegmentControl.selectedSegmentIndex = 0
+        } else {
+            unitSegmentControl.selectedSegmentIndex = 1
+            
+            oilChangeSegmentControl.setTitle("\(UnitConverter.milesToKilometers(5000)) km", forSegmentAtIndex: 0)
+            oilChangeSegmentControl.setTitle("\(UnitConverter.milesToKilometers(8000)) km", forSegmentAtIndex: 1)
+            transmissionSegmentControl.setTitle("\(UnitConverter.milesToKilometers(30000)) km", forSegmentAtIndex: 0)
+            transmissionSegmentControl.setTitle("\(UnitConverter.milesToKilometers(60000)) km", forSegmentAtIndex: 1)
+        }
         
-        let entityDescripition = NSEntityDescription.entityForName("Setting", inManagedObjectContext: managedObjectContext!)
-        userSetting = Setting(entity: entityDescripition!, insertIntoManagedObjectContext: managedObjectContext)
+        if setting?.oilChangeFrequency == 5000 {
+            oilChangeSegmentControl.selectedSegmentIndex = 0
+        } else {
+            oilChangeSegmentControl.selectedSegmentIndex = 1
+        }
         
-        //userSetting.unit = setting?.unit
+        if setting?.transmissionFluidFrequency == 30000 {
+            transmissionSegmentControl.selectedSegmentIndex = 0
+        } else {
+            transmissionSegmentControl.selectedSegmentIndex = 1
+        }
         
-//        userSetting.oilChangeFrequency = setting?.oilChangeFrequency
-//        userSetting.transmissionFluidFrequency = setting?.transmissionFluidFrequency
-        
-        managedObjectContext?.save(nil)
-
-
     }
+    
 }
